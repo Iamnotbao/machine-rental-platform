78b1e439210550706b03d5d583e09dd18eeda921
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-25 14:55:54 +0700

    style(client): refine server rental landing and booking UI

--- apps/client/src/pages/Home/Home.tsx
+++ apps/client/src/pages/Home/Home.tsx
@@ -1,26 +1,11 @@
-import { AboutCompany } from './sections/AboutCompany/AboutCompany';
-import { CallToAction } from './sections/CTA/CTA';
 import { FeaturedMachines } from './sections/FeaturedMachines/FeaturedMachines';
 import { Hero } from './sections/Hero/Hero';
-import { HowItWorks } from './sections/HowItWorks/HowItWorks';
-import { Newsletter } from './sections/Newsletter/Newsletter';
-import { Statistics } from './sections/Statistics/Statistics';
-import { Testimonials } from './sections/Testimonials/Testimonials';
-import { WhyChooseUs } from './sections/WhyChooseUs/WhyChooseUs';
-import styles from './Home.module.css';
 
 export function HomePage() {
   return (
-    <div className={styles.home}>
+    <main>
       <Hero />
-      <Statistics />
       <FeaturedMachines />
-      <HowItWorks />
-      <WhyChooseUs />
-      <AboutCompany />
-      <Testimonials />
-      <CallToAction />
-      <Newsletter />
-    </div>
+    </main>
   );
 }
