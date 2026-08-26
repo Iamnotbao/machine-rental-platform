b2ac6912c7819dfcae4d2b7d218513aa11a6aff7
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2026-08-26 10:05:33 +0700

    fix(client): remove remaining duplicate RouterProvider

--- apps/client/src/app/router/index.tsx
+++ apps/client/src/app/router/index.tsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/app/router/routes';
-
 export function AppRouter() {
-  return <RouterProvider router={router} />;
+  return null;
 }
+
+export default AppRouter;
+export { router } from './routes';
